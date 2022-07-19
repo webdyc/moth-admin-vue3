742929fef065585f153e415bf934a7c122ed2e0e
董宇成 <dongyucheng@unioncast.cn>
2022-07-19 15:53:58 +0800

    升级版本

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
 </template>
 <script>
 import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN'
-import moment from 'moment'
-import 'moment/locale/zh-cn'
+// import moment from 'moment'
+// import 'moment/locale/zh-cn'
 
-moment.locale('zh-cn')
+// moment.locale('zh-cn')
 export default {
   name: 'App',
   data() {
