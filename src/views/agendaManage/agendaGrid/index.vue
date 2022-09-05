98746cc65db33517c8ff9a748d1f02b0f1d2034c
郝荣 <haorong@unioncast.cn>
2022-09-05 17:23:53 +0800

    提交

--- src/views/agendaManage/agendaGrid/index.vue
+++ src/views/agendaManage/agendaGrid/index.vue
@@ -2,7 +2,7 @@
  * @Author: 郝荣 haorong@unioncast.cn
  * @Date: 2022-08-24 14:48:42
  * @LastEditors: 郝荣 haorong@unioncast.cn
- * @LastEditTime: 2022-08-25 11:42:10
+ * @LastEditTime: 2022-08-30 16:53:41
  * @FilePath: \moth-admin-vue3\src\views\agendaManage\agendaGrid\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -33,7 +33,7 @@
 </template>
 <script>
 import { ref, onMounted, createVNode } from 'vue';
-import { Calendar } from '@fullcalendar/core';
+import { Calendar } from '@fullcalendar/core';  //日历组件
 import { reactive, toRefs, toRaw } from '@vue/reactivity'
 import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';  // 视图
 import interactionPlugin from '@fullcalendar/interaction';  // 拖动
@@ -80,27 +80,28 @@ export default ({
         selectable: true,  // 选择区域
         locale: 'zh-cn',  // 语言
         editable: true,   // 开启拖动
-        resources: [
+        resources: [   // 表头标题
           { id: 'a', title: '场馆1' },
           { id: 'b', title: '场馆2' },
           { id: 'c', title: '场馆3' },
           { id: 'd', title: '场馆4' }
         ],  // 自定义表头
         // events: 'https://fullcalendar.io/api/demo-feeds/events.json?with-resources=5&single-day',   // 数据接口
-        events: stateData.events,
+        events: stateData.events, // 初始化数据
         select: function (info) {
+          // 日历选择事件
           let rgb = `rgba(${Math.floor(Math.random() * 225)},${Math.floor(Math.random() * 225)},${Math.floor(Math.random() * 225)},1)`
           let eventsData = {
-            resourceId: info.resource._resource.id,
-            title: '',
-            start: info.startStr,
-            end: info.endStr,
-            color: rgb,
-            id: new Date().getTime() + Math.floor(Math.random() * 100)
+            resourceId: info.resource._resource.id, // 对应日历事件标头数据id
+            title: '',// 日历事件标题
+            start: info.startStr,//日历事件开始时间
+            end: info.endStr,  //日历事件结束时间
+            color: rgb, // 日历事件背景颜色
+            id: new Date().getTime() + Math.floor(Math.random() * 100) //随机id以方便操作修改数据
           }
-          stateData.events.push(eventsData)
-          stateData.calendar.addEvent(eventsData);
-          stateData.calendarData = eventsData
+          stateData.events.push(eventsData)  // 备用数据暂时无用
+          stateData.calendar.addEvent(eventsData);  // 添加数据
+          stateData.calendarData = eventsData  // 暂存数据以方便数据编辑后回填
           stateData.updateFormVisible = true;
         },
         eventClick: function (info) {
@@ -121,12 +122,7 @@ export default ({
 
       stateData.calendar.render();
     })
-    const handleSubmit = (start) => {
-      console.log(start);
-
-      console.log(stateData.events);
-      return stateData.events
-    }
+    // 弹出模态框确定事件
     const handleOk = (values) => {
       validate()
         .then(res => {
@@ -154,6 +150,7 @@ export default ({
       // })
       // console.log(EventsData);
     }
+    // 模态框取消事件
     const handleCancel = () => {
       var event = stateData.calendar.getEventById(stateData.calendarData.id)
       if (!event.title) {
@@ -161,6 +158,7 @@ export default ({
       }
       closeForm()
     }
+    // 删除日程数据
     const calenderDelete = () => {
       stateData.popoverVisible = false
       Modal.confirm({
@@ -187,7 +185,6 @@ export default ({
     }
     return {
       ...toRefs(stateData),
-      handleSubmit,
       handleOk,
       handleCancel,
       validateInfos,
